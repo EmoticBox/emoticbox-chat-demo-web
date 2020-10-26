cd85a3ba1bb7014716b8ecffc976bba15f49661e
Macmini <macmini@Macminiui-Mac-mini.local>
2020-10-26 17:50:09 +0900

    padding message, tabindex for keydown

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import styled from 'styled-components';
 import PhoneImage from './Img/phone_mock_up.png'
 import BackCircle from './Img/back_circle.png'
 import Emoticon from './Img/emoticon.png'
+import Connections from './Img/connections.png'
+import Search from './Img/search.png'
 
 // components
 import MessageBox from './Component/MessageBox.js'
@@ -67,10 +69,18 @@ class App extends Component {
       }),
     });
   }
+  _handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      this._sendMessage();
+    }
+  }
   // 보내는 메세지 추가
   _sendMessage = () => {
     const { message, messageList, selectedId, visible } = this.state;
     const date = new Date();
+    // 입력한 문자열이 없으면 return
+
+    if (message === "" && visible === false ) return;
     console.log("selected " + selectedId);
     this.setState({
       messageList: messageList.concat({
@@ -107,7 +117,8 @@ class App extends Component {
     isOpen = !isOpen;
   }
   _onClickEmoticon = (index) => {
-    const {visible} = this.state;
+    const {visible,selectedId} = this.state;
+    if (selectedId === index) this._sendMessage();
     this.setState({
       visible: !visible,
       selectedId: index,
@@ -121,9 +132,9 @@ class App extends Component {
     return (
       <Background>
         <Viewer>
-
           <Header>
-            <Title>Default</Title>
+            <LineA><LabelLeft>9 : 45</LabelLeft><ConnectionIcons/></LineA>
+            <LineB><Arrows>&lt;</Arrows><LabelLeft>Developer JH</LabelLeft><LabelRight>...</LabelRight><SearchIcon/></LineB>
           </Header>
 
           <Section id="section">
@@ -137,15 +148,16 @@ class App extends Component {
             </ChatList>
           </Section>
           <EmoticonPreview visible={this.state.visible} index={this.state.selectedId}></EmoticonPreview>
-          <Footer id="footer">
+          <Footer id="footer" onKeyDown={this._handleKeyDown} tabIndex="0">
             <InputMessage
               placeholder="메세지를 입력해주세요."
               onChange={this._handleChange}
               value={message}
+              
             />
             <EmoticonButton onClick={this._handleEmoticonView}><ButtonImg src={Emoticon} /></EmoticonButton>
             <SendButton onClick={this._sendMessage}>전송</SendButton>
-            <EmoticonList onClick={this._onClickEmoticon}/>
+            <EmoticonList onClick={this._onClickEmoticon} />
           </Footer>
 
         </Viewer>
@@ -153,6 +165,21 @@ class App extends Component {
     );
   }
 }
+// Lines
+const LineA = styled.div`
+  
+`
+const LineB = styled.div`
+  clear: both;
+`
+// Connection Icons
+const ConnectionIcons = styled.div`
+  width: 68px;
+  height: 16px;
+  background-image: url(${Connections});
+  margin: 10px 10px 10px 10px;
+  float:right;
+`
 // BackGround
 const Background = styled.div`
   width: 650px;
@@ -175,11 +202,40 @@ const Header = styled.div`
   height: 74px;
   background: #2DD5B2;
   margin: 0px;
+  border-radius: 7px 7px 0px 0px;
 `
-const Title = styled.h1`
+const LabelLeft= styled.div`
   color:#F2F2F2;
   margin: 0px;
   text-align: center;
+  float:left;
+  margin: 10px 10px 10px 20px;
+  font-family: SF Pro Text;
+  font-size: 15px;
+  line-height: 18px;
+`
+const LabelRight = styled.div`
+  color:#F2F2F2;
+  margin: 0px;
+  text-align: center;
+  float:right;
+  margin: 3px 20px 10px 0px;
+  font-size: 16px;
+  line-height: 20px;
+`
+const SearchIcon = styled.div`
+  width: 12px;
+  height: 12px;
+  float: right;
+  margin: 10px;
+  background-image: url(${Search})
+`
+const Arrows = styled.div`
+  color: #f2f2f2;
+  font-size: 20px;
+  line-height: 18px;
+  margin: 10px 10px 10px 20px;
+  float: left;
 `
 
 // Section
@@ -218,22 +274,20 @@ const Footer = styled.div`
   background: #ffffff;
   border-radius: 0px 0px 7px 7px;
   clear: right;
-
+  outline:none;
 `
 const InputMessage = styled.input`
   float:left;
   margin-left: -1px;
-  width: 314px;
+  width: 214px;
   height: 36px;
   line-height: 20px;
   font-size: 15px;
   outline: none;
   padding-left: 10px;
+  padding-right: 100px;
   border: 1px solid #E5E5E5;
 
-  font-family: Noto Sans KR;
-  font-style: normal;
-  font-weight: 500;
   font-size: 12px;
   line-height: 17px;
   display: flex;
