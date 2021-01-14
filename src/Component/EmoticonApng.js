a95868bf152a9eb725448d32f7cbde3aa5ff2bc3
mac <mac@macui-MacBookPro.local>
2021-01-14 16:53:08 +0900

    image hover error fix

--- src/Component/EmoticonApng.js
+++ src/Component/EmoticonApng.js
@@ -28,8 +28,8 @@ const EmoticonSet = styled.a`
 `
 const EmoticonThm = styled.img`
   display: inline;
-  height: 80px;
-  padding: 0px;
+  height: 76px;
+  padding 7px;
   object-fit: cover;
 `
 const EmoticonImg = styled.img`
